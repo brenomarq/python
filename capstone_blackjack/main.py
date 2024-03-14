a3725e4bd966c756b6aa3120e57278e07a3dae30
brenomarq <brenomarques516@gmail.com>
2024-03-14 12:00:44 -0300

    Finish blackjack game project

--- capstone_blackjack/main.py
+++ capstone_blackjack/main.py
@@ -27,8 +27,27 @@ from art import logo
 from os import system
 from random import choice
 
-def check_scores(user_score: int, computer_score: int):
-    ...
+def check_scores(user_score: int, computer_score: int) -> str:
+    """Return a string telling if the user won or lost against the computer."""
+    message = ""
+
+    if computer_score == user_score:
+        message = "It's a draw. 😐"
+    elif user_score == 21:
+        message = "Blackjack! You win. 😮"
+    elif user_score > 21 and computer_score > 21:
+        if user_score < computer_score:
+            message = "You win! 🤩"
+        else:
+            message = "You lose! 😟"
+    elif user_score > 21 and computer_score <= 21:
+        message = "You lose! 😟"
+    elif computer_score > 21 or user_score > computer_score:
+        message = "You win! 🤩"
+    else:
+        message = "You lose! 😟"
+
+    return message
 
 def game() -> None:
     user_cards = [choice(cards) for _ in range(2)]
@@ -39,9 +58,33 @@ def game() -> None:
         user_score = sum(user_cards)
         computer_score = sum(computer_cards)
 
-        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"\nYour cards: {user_cards}, current score: {user_score}")
         print(f"Computer's first card: {computer_cards[0]}")
 
+        if computer_score <= 16:
+            computer_cards.append(choice(cards))
+            computer_score += computer_cards[-1]  # Solves bug
+
+        if user_score < 21:
+            get_card = input("Type 'y' to get another cards, or 'n' to pass: ")
+
+            if get_card == "y":
+                user_cards.append(choice(cards))
+                continue
+
+        print(f"\nYour cards: {user_cards}, computer's cards: {computer_cards}")
+        print(f"Your score: {user_score}, computer's score: {computer_score}")
+        print(check_scores(user_score, computer_score))
+
+        keep_playing = input("\nDo you want to keep playing? Type 'y' or 'n': ")
+
+        if keep_playing == "y":
+            system("clear")
+            game()
+
+        should_continue = False
+
+    return
 
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -51,3 +94,4 @@ if wanna_play == "y":
     system("clear")
     print(logo)
     game()
+    print("Thank you for playing our game!")
