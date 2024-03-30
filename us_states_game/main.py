931b3a1870b22ddefba800b2770056f509d61745
brenomarq <brenomarques516@gmail.com>
2024-03-30 09:29:04 -0300

    Add list comprehension into the project

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -13,19 +13,21 @@ states_data = pandas.read_csv("./50_states.csv")
 all_states = states_data.state.to_list()
 
 map_writter = MapWritter()
+guessed_states = []
 score = 0
 while len(all_states) > 0:
     try:
         user_guess = screen.textinput(title=f"{score}/50 Guess the State", prompt="Type the name of any state:").title()
 
         if user_guess == "Exit":
-            missed_states = pandas.DataFrame({"state": all_states})
-            missed_states.to_csv("./missed_states.csv")
+            missing_states = [state for state in all_states if state not in guessed_states]
+            new_data = pandas.DataFrame({"state": missing_states})
+            new_data.to_csv("./missed_states.csv")
             break
 
         if user_guess in all_states:
             state = states_data[states_data.state == user_guess].values[0]
-            all_states.remove(user_guess)
+            guessed_states.append(user_guess)
 
             map_writter.write_answer(name=user_guess, position=(state[1], state[2]))
             score += 1
