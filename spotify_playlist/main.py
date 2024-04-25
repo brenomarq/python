09fd6663f5529f30fa718b810f144fe020c49de8
brenomarq <brenomarques516@gmail.com>
2024-04-25 12:19:45 -0300

    Finish Playlist Creator Bot Project

--- spotify_playlist/main.py
+++ spotify_playlist/main.py
@@ -2,10 +2,21 @@ from music_scraper import MusicScraper
 from playlist_manager import PlaylistManager
 
 music_scraper = MusicScraper()
+playlist_manager = PlaylistManager()
 
-chosen_year = input("What year would you like to travel to? (Format: YYYY-MM-DD)\n")
+chosen_date = input("What year would you like to travel to? (Format: YYYY-MM-DD)\n")
 
-for title in music_scraper.search_songs(chosen_year):
-    print(title)
+songs = music_scraper.search_songs(chosen_date)
+year = chosen_date.split("-")[0]
 
+songs_uri = []
+for song in songs:
+    name = f"track:{song} year:{year}"
+
+    uri = playlist_manager.search_song(name)
+    if uri:
+        songs_uri.append(uri)
+
+playlist_name = f"{chosen_date} Billboard 100"
+playlist_manager.create_playlist(playlist_name=playlist_name, songs=songs_uri)
 
--- spotify_playlist/playlist_manager.py
+++ spotify_playlist/playlist_manager.py
@@ -1,4 +1,45 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+ID = os.environ.get("CLIENT_ID")
+SECRET = os.environ.get("CLIENT_SECRET")
+URI = "http://example.com"
+SCOPE = "playlist-modify-private"
+
 class PlaylistManager:
-    """This class is responsible for communicating with the spotify API in order to create playlists."""
     def __init__(self) -> None:
-        pass
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=ID,
+            client_secret=SECRET,
+            redirect_uri=URI,
+            scope=SCOPE,
+            show_dialog=True,
+            cache_path=".cache"
+        ))
+
+        self.user_id = self.sp.current_user()["id"]
+
+    def search_song(self, name: str) -> str | None:
+        """Seach for a song and return its uri on spotify."""
+        result = self.sp.search(q=name, type="track")
+
+        try:
+            uri = result["tracks"]["items"][0]["uri"]
+            return uri
+
+        except IndexError:
+            return None
+
+    def create_playlist(self, playlist_name: str, songs: list[str]) -> None:
+        """Create a playlist on Spotify with all the songs included in the songs list."""
+        playlist = self.sp.user_playlist_create(
+            user=self.user_id,
+            name=playlist_name,
+            public=False
+        )
+
+        id = playlist["id"]
+        self.sp.playlist_add_items(playlist_id=id, items=songs)
