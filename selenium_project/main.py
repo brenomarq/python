343cd897dc748840331301d9c050e992f93f16ac
brenomarq <brenomarques516@gmail.com>
2024-05-02 16:04:30 -0300

    Finish cookie clicker bot project

--- selenium_project/main.py
+++ selenium_project/main.py
@@ -1,31 +1,26 @@
-from datetime import datetime
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def check_time(last_time: datetime) -> datetime:
-    time_now = datetime.now()
-    diff_seconds = (time_now - last_time).seconds
+def search_items() -> list:
+    items = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+    prices = driver.find_elements(By.CSS_SELECTOR, value="#store b")
 
-    if diff_seconds >= 5:
-        print("5 seconds have passed.")
-        buy_stuff()
-        return time_now
+    store_items = []
+    for i, price in enumerate(prices):
+        text_price = price.text
 
-    return last_time
+        if text_price:
+            cost = int(text_price.split("-")[-1].replace(",", ""))
+            item = items[i]
 
+            store_items.append({
+                "item": item,
+                "price": cost,
+            })
 
-def buy_stuff() -> None:
-    money = int(driver.find_element(By.CSS_SELECTOR, "#money").text)
-    items = driver.find_elements(By.CSS_SELECTOR, ".grayed")
-    prices = driver.find_elements(By.CSS_SELECTOR, ".grayed b")
+    return store_items[::-1]
 
-    print(len(items))
-    print(len(prices))
-
-    costs = []
-    for price in prices:
-        cost = int(price.text.split(" ")[-1].replace(",", ""))
-        costs.append(cost)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -35,7 +30,25 @@ driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
 
-last_time = datetime.now()
+timeout = time.time() + 5
+five_min = time.time() + 5*60
+
 while True:
-    last_time = check_time(last_time)
     cookie.click()
+
+    if time.time() > five_min:
+        cookie_rate = driver.find_element(By.ID, value="cps").text
+        print(cookie_rate)
+        driver.quit()
+        break
+
+    if time.time() > timeout:
+        money = int(driver.find_element(By.ID, value="money").text.replace(",", ""))
+
+        store_items = search_items()
+        for item in store_items:
+            if money >= item["price"]:
+                item["item"].click()
+                break
+
+        timeout = time.time() + 5
