8e4ec6df96e4e14611bb353e7d268990949070fe
brenomarq <brenomarques516@gmail.com>
2024-05-07 11:02:03 -0300

    Finish login and tweet functionalities

--- twitter_complaint/main.py
+++ twitter_complaint/main.py
@@ -1,44 +1,25 @@
 import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from dotenv import load_dotenv, find_dotenv
 from twitterbot import InternetComplaintTwitterBot
 load_dotenv(find_dotenv())
 
 SPEED_URL = "https://www.speedtest.net/"
-TWITTER_URL = "https://twitter.com/home"
 TWITTER_EMAIL = os.environ.get("EMAIL")
 TWITTER_PASSWORD = os.environ.get("PASSWORD")
-PROMISED_DOWNLOAD = 150
-PROMISED_UPLOAD = 50
+PROMISED_DOWNLOAD = 150.0
+PROMISED_UPLOAD = 50.0
 
 twitter_bot = InternetComplaintTwitterBot()
 
 is_slow = twitter_bot.is_internet_slow(url=SPEED_URL, speeds=[PROMISED_DOWNLOAD, PROMISED_UPLOAD])
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach", True)
+if is_slow:
+    print("The internet is slow.")
 
-# driver = webdriver.Chrome(options=chrome_options)
-# driver.get(url=SPEED_URL)
+    twitter_bot.twitter_login(
+        email=TWITTER_EMAIL,
+        password=TWITTER_PASSWORD,
+        user="brenomp516"
+    )
 
-# # Test the internet speed
-# button = driver.find_element(By.CLASS_NAME, value="js-start-test")
-# button.click()
-
-# time.sleep(45)
-
-# # Get the results and compare to the promised by the provider
-# download_speed: float = float(driver.find_element(By.CLASS_NAME, value="download-speed").text)
-# upload_speed: float = float(driver.find_element(By.CLASS_NAME, value="upload-speed").text)
-
-# if download_speed < PROMISED_DOWNLOAD or upload_speed < PROMISED_UPLOAD:
-#     driver.get(url=TWITTER_URL)
-#     time.sleep(3)
-
-#     # Logs into twitter
-#     login_btn = driver.find_element(By.CLASS_NAME, value="nsm7Bb-HzV7m-LgbsSe")
-#     login_btn.click()
-
-#     email_input = driver.find_element(By.CSS_SELECTOR, value="input.whsOnd")
-#     email_input.send_keys(TWITTER_EMAIL)
+    twitter_bot.send_tweet()
--- twitter_complaint/twitterbot.py
+++ twitter_complaint/twitterbot.py
@@ -1,6 +1,9 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+TWITTER_URL = "https://twitter.com/home"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -8,6 +11,9 @@ chrome_options.add_experimental_option("detach", True)
 class InternetComplaintTwitterBot:
     def __init__(self) -> None:
         self._driver = webdriver.Chrome(options=chrome_options)
+        self.down = 0
+        self.up = 0
+        self.promised = [10, 10]
 
     def is_internet_slow(self, url: str, speeds: list[int]) -> bool:
         self._driver.get(url)
@@ -23,14 +29,51 @@ class InternetComplaintTwitterBot:
         download_speed: float = float(self._driver.find_element(By.CLASS_NAME, value="download-speed").text)
         upload_speed: float = float(self._driver.find_element(By.CLASS_NAME, value="upload-speed").text)
 
+        self.down = download_speed
+        self.up = upload_speed
+        self.promised = speeds
+
         if download_speed < speeds[0] or upload_speed < speeds[1]:
             return True
 
         return False
 
-    def send_tweet(self, url: str) -> None:
-        pass
+    def twitter_login(self, email: str, password: str, user: str) -> None:
+        self._driver.get(url=TWITTER_URL)
+        time.sleep(5)
 
+        email_input = self._driver.find_element(By.NAME, value="text")
+        email_input.send_keys(email, Keys.ENTER)
+        time.sleep(5)
 
+        user_input = self._driver.find_element(By.NAME, value="text")
+        user_input.send_keys(user, Keys.ENTER)
+        time.sleep(5)
 
+        password_input = self._driver.find_element(By.NAME, value="password")
+        password_input.send_keys(password, Keys.ENTER)
+        time.sleep(10)
 
+        return None
+
+    def send_tweet(self) -> None:
+        """This method must be called only after twitter login."""
+        compose_btn = self._driver.find_element(
+            By.XPATH,
+            value='//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a'
+        )
+        compose_btn.click()
+        time.sleep(3)
+
+        text_area = self._driver.find_element(
+            By.XPATH,
+            value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div/div/div/div/div/span'
+        )
+        text_area.send_keys(f"[This a bot text]\nHey Internet Provider! Why is my internet speed {self.down}down/\
+        {self.up}up, when I pay for {self.promised[0]}down/{self.promised[1]}up?")
+
+        post_btn = self._driver.find_element(
+            By.XPATH,
+            value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div/div/div[4]'
+        )
+        post_btn.click()
