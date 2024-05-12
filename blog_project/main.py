ffa9182b66c35a55ed6dad8cfb1f03cb836c8d3a
brenomarq <brenomarques516@gmail.com>
2024-05-12 17:00:38 -0300

    Finish blog project part 1

--- blog_project/main.py
+++ blog_project/main.py
@@ -16,6 +16,8 @@ def fetch_posts() -> list[Post]:
         current_post = Post(post)
         posts.append(current_post)
 
+    return posts
+
 
 app = Flask(__name__)
 
@@ -24,7 +26,16 @@ app = Flask(__name__)
 def home():
     all_posts = fetch_posts()
 
-    return render_template('index.html', year=all_posts)
+    return render_template('index.html', posts=all_posts)
+
+
+@app.route('/posts/<int:id>')
+def get_post(id: int):
+    all_posts = fetch_posts()
+
+    post: Post = list(filter(lambda post: post.id == id, all_posts))[0]
+
+    return render_template('post.html', post=post)
 
 
 if __name__ == "__main__":
